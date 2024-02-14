94b91ac22675df17e99159f8223f663d768fd510
rafael gonzalez <rafael.gonzalezm94@gmail.com>
2024-02-14 09:34:52 -0600

    og tags

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import logo from '../assets/img/Logo.svg';
+
+export const Header = () => {
+  return (
+    <header>
+        <img src={logo} alt='logo' />
+    </header>
+  )
+}
